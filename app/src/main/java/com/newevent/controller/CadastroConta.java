7307c3c840f819511d0c4e582609c8830f98d56c
antonio02 <antonio.filho.01@hotmail.com>
2018-07-28 21:51:55 -0300

    [Utils] Criando validador para criar nova conta

--- app/src/main/java/com/newevent/controller/CadastroConta.java
+++ app/src/main/java/com/newevent/controller/CadastroConta.java
@@ -2,17 +2,29 @@ package com.newevent.controller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.newevent.R;
 
 public class CadastroConta extends AppCompatActivity {
 
-    FirebaseAuth auth = FirebaseAuth.getInstance();
+    private FirebaseAuth auth = FirebaseAuth.getInstance();
+    private EditText edtxtEmail;
+    private EditText edtxtSenha;
+    private EditText edtxtConfirmeSenha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro_conta);
+    }
+
+    public void cancelar(View view) {
+        finish();
+    }
+
+    public void finalizar(View view) {
     }
 }

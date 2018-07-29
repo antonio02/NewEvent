1fa7b3eaeadd8f8bf3ee04c0dce264344edac2f1
antonio02 <antonio.filho.01@hotmail.com>
2018-07-28 21:43:10 -0300

    [Controller][Activity] Criando layout activity CadastroConta

--- app/src/main/java/com/newevent/controller/CadastroConta.java
+++ app/src/main/java/com/newevent/controller/CadastroConta.java
@@ -3,9 +3,12 @@ package com.newevent.controller;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.newevent.R;
 
 public class CadastroConta extends AppCompatActivity {
+
+    FirebaseAuth auth = FirebaseAuth.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

8654fee4e981d7c06b70869dba470cad9d3ffe6a
MicaelLeal <mycaellmoura@hotmail.com>
2018-08-01 02:58:37 -0300

    Tela formulario de criação para evento

---
+++ app/src/main/java/com/newevent/controller/CriarEvento.java
@@ -0,0 +1,15 @@
+package com.newevent.controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.newevent.R;
+
+public class CriarEvento extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_criar_evento);
+    }
+}

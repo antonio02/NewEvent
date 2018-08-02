88f530bbf2ba3c98293079f24eb59699d30cfeea
MicaelLeal <mycaellmoura@hotmail.com>
2018-08-02 02:06:06 -0300

    Update no modelo 'Local' e implementação de um validador para o mesmo

---
+++ app/src/main/java/com/newevent/utils/CriarLocalValidador.java
@@ -0,0 +1,45 @@
+
+package com.newevent.utils;
+
+public class CriarLocalValidador {
+
+    public static final int VALIDO = 0;
+    public static final int ENDERECO_INVALIDO = 1;
+    public static final int BAIRRO_INVALIDO = 2;
+    public static final int CIDADE_INVALIDO = 3;
+    public static final int UF_INVALIDO = 4;
+
+    public static int validarLocal(String endereco, String bairro,
+                                   String cidade, String uf) {
+        if (!isValidoEndereco(endereco)) {
+            return ENDERECO_INVALIDO;
+        }
+        if (!isvalidoBairro(bairro)) {
+            return BAIRRO_INVALIDO;
+        }
+        if (!isvalidoCidade(cidade)) {
+            return CIDADE_INVALIDO;
+        }
+        if (!isvalidoUf(uf)) {
+            return UF_INVALIDO;
+        }
+        return VALIDO;
+    }
+
+    private static boolean isValidoEndereco(String endereco) {
+        return endereco != null && !endereco.trim().isEmpty();
+    }
+
+    private static boolean isvalidoBairro(String bairro) {
+        return bairro != null && !bairro.trim().isEmpty();
+    }
+
+    private static boolean isvalidoCidade(String cidade) {
+        return cidade != null && !cidade.trim().isEmpty();
+    }
+
+    private static boolean isvalidoUf(String uf) {
+        return uf != null && uf.trim().length() == 2;
+    }
+
+}

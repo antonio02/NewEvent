006299a85a65bfe41dec60688e28f8ebd2b4b3cf
antonio02 <antonio.filho.01@hotmail.com>
2018-08-19 18:57:45 -0300

    [Development] modificando a classe local para passar nos testes

--- app/src/main/java/com/newevent/model/Local.java
+++ app/src/main/java/com/newevent/model/Local.java
@@ -9,10 +9,10 @@ public class Local {
     private String complemento;
 
     public Local(String endereco, String bairro, String cidade, String uf) {
-        this.endereco = endereco;
-        this.bairro = bairro;
-        this.cidade = cidade;
-        this.uf = uf;
+        this.endereco = validarEndereco(endereco);
+        this.bairro = validarBairro(bairro);
+        this.cidade = validarCidade(cidade);
+        this.uf = validarUF(uf);
     }
 
     public String getEndereco() {
@@ -36,6 +36,38 @@ public class Local {
     }
 
     public void setComplemento(String complemento) {
+        if(complemento.trim().isEmpty()){
+            this.complemento = null;
+            return;
+        }
         this.complemento = complemento;
+    }
+
+    private String validarEndereco(String endereco){
+        if(endereco == null || endereco.trim().isEmpty()){
+            throw new IllegalArgumentException("Endereço vazio");
+        }
+        return endereco;
+    }
+
+    private String validarBairro(String bairro){
+        if(bairro == null || bairro.trim().isEmpty()){
+            throw new IllegalArgumentException("Bairro vazio");
+        }
+        return bairro;
+    }
+
+    private String validarCidade(String cidade){
+        if(cidade == null || cidade.trim().isEmpty()){
+            throw new IllegalArgumentException("Cidade vazia");
+        }
+        return cidade;
+    }
+
+    private String validarUF(String uf){
+        if(uf == null || uf.trim().contains(" ") || uf.trim().length() != 2){
+            throw new IllegalArgumentException("UF vazia ou invalido");
+        }
+        return uf;
     }
 }

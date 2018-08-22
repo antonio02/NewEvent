783b5ed740eef62afe5af4292b9709de288e3066
antonio02 <antonio.ads0394@gmail.com>
2018-08-22 17:06:29 -0300

    [util] util de data minima para publicacao

--- app/src/main/java/com/newevent/utils/DataUtil.java
+++ app/src/main/java/com/newevent/utils/DataUtil.java
@@ -5,9 +5,16 @@ import java.util.Date;
 public class DataUtil {
 
     //Data atual + 6 horas
-    public static Date getMinimaEvento(){
+    public static Date getMinimaCriarEvento(){
         Date data = new Date();
         data.setTime(data.getTime() + 21600000);
+        return data;
+    }
+
+    //Data atual + 2 horas
+    public static Date getMinimaPublicarEvento(){
+        Date data = new Date();
+        data.setTime(data.getTime() + 7200000);
         return data;
     }
 

373b760aa32f4d10612a54862627c918de5300d6
MicaelLeal <mycaellmoura@hotmail.com>
2018-08-23 12:15:19 -0300

    Fnalizando listagem das atividadem em meu evento

--- app/src/main/java/com/newevent/adapter/AtividadeEmMeuEvetoRvAdapter.java
+++ app/src/main/java/com/newevent/adapter/AtividadeEmMeuEvetoRvAdapter.java
@@ -14,6 +14,7 @@ import com.newevent.dao.atividade.interfaces.GetAtividadesDoEventoRealTimeListen
 import com.newevent.model.Atividade;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.List;
 
 public class AtividadeEmMeuEvetoRvAdapter
@@ -30,6 +31,7 @@ public class AtividadeEmMeuEvetoRvAdapter
     public AtividadeEmMeuEvetoRvAdapter(Context contexto, String eventoUid) {
         this.contexto = contexto;
         this.eventoUid = eventoUid;
+        atividades = new ArrayList<>();
         getAtividades = new GetAtividadesDoEventoRealtime(eventoUid, this);
     }
 

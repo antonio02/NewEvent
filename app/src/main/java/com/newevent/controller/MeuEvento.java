a98db6c4918e37f65aada5ff03b2ca1bb1997f90
MicaelLeal <mycaellmoura@hotmail.com>
2018-08-23 04:58:40 -0300

    Mostrando dados básicos em meuEvento

--- app/src/main/java/com/newevent/controller/MeuEvento.java
+++ app/src/main/java/com/newevent/controller/MeuEvento.java
@@ -3,16 +3,43 @@ package com.newevent.controller;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.newevent.R;
 import com.newevent.dao.evento.GetEventoRealtime;
 import com.newevent.dao.evento.interfaces.GetEventoRealtimeListener;
 import com.newevent.model.Evento;
+import com.newevent.model.Local;
 import com.newevent.utils.UidUtil;
 
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
 public class MeuEvento extends AppCompatActivity implements GetEventoRealtimeListener {
+
+
+    private EditText edTipoEvento;
+    private EditText edNomeEvento;
+    private EditText edDataInicioEvento;
+    private EditText edDataFimEvento;
+
+    private TextView txtEnderecoLocalEvento;
+    private TextView txtBairroLocalEvento;
+    private TextView txtCidadeLocalEvento;
+    private TextView txtUfLocalEvento;
+    private TextView txtComplementoLocalEvento;
+
+    private CardView cardLocal;
+    private Button btnNovaAtividade;
+    private Button btnEditar;
+    private LinearLayout btnsFinalizarCancelar;
+
 
     private String eventoUid;
     private Evento evento;
@@ -24,12 +51,92 @@ public class MeuEvento extends AppCompatActivity implements GetEventoRealtimeLis
         setContentView(R.layout.activity_meu_evento);
 
         carregarEvento();
+        inicializarViews();
     }
 
     @Override
     protected void onDestroy() {
         getEvento.stop();
         super.onDestroy();
+    }
+
+    @Override
+    public void onUpdate(Evento evento) {
+        this.evento = evento;
+        mostrarDadosDoEvento();
+        listarAtividades();
+    }
+
+    private void listarAtividades() {
+
+    }
+
+    private void mostrarDadosDoEvento() {
+        if(evento == null) {
+            Toast.makeText(this, "Evento não carregou!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        Local local = evento.getLocal();
+        SimpleDateFormat format = new SimpleDateFormat("dd - MMMM - yyyy",
+                Locale.getDefault());
+
+        edNomeEvento.setText(evento.getNome());
+        edTipoEvento.setText(evento.getTipo());
+
+        txtEnderecoLocalEvento.setText(local.getEndereco());
+        txtBairroLocalEvento.setText(local.getBairro());
+        txtCidadeLocalEvento.setText(local.getCidade());
+        txtUfLocalEvento .setText(local.getUf());
+        txtComplementoLocalEvento.setText(local.getComplemento());
+
+        edDataInicioEvento.setText(format.format(evento.getDataInicio()));
+
+        if (evento.getDataTermino() != null) {
+            edDataFimEvento.setText(format.format(evento.getDataTermino()));
+        }
+    }
+
+    @Override
+    public void onDelete() {
+        finish();
+    }
+
+    public void abrirCriarAtividade(View view) {
+        Intent it = new Intent(this, CriarAtividade.class);
+        it.putExtra("evento_uid", eventoUid);
+        startActivity(it);
+    }
+
+    public void abilitarEdicao(View view) {
+
+        btnEditar.setVisibility(View.GONE);
+        btnNovaAtividade.setVisibility(View.GONE);
+        btnsFinalizarCancelar.setVisibility(View.VISIBLE);
+
+        edTipoEvento.setFocusableInTouchMode(true);
+        edNomeEvento.setFocusableInTouchMode(true);
+
+        atribuirEscutadorEmEdData();
+    }
+
+    private void atribuirEscutadorEmEdData() {
+
+    }
+
+    public void finalizarEdicao(View view) {
+    }
+
+    public void cancelarEdicao(View view) {
+
+        btnsFinalizarCancelar.setVisibility(View.GONE);
+        btnNovaAtividade.setVisibility(View.VISIBLE);
+        btnEditar.setVisibility(View.VISIBLE);
+
+        edTipoEvento.setFocusable(false);
+        edNomeEvento.setFocusable(false);
+
+        mostrarDadosDoEvento();
     }
 
     private void carregarEvento() {
@@ -47,28 +154,21 @@ public class MeuEvento extends AppCompatActivity implements GetEventoRealtimeLis
         getEvento = new GetEventoRealtime(eventoUid, this);
     }
 
-    public void abrirCriarAtividade(View view) {
-        Intent it = new Intent(this, CriarAtividade.class);
-        it.putExtra("evento_uid", eventoUid);
-        startActivity(it);
-    }
+    private void inicializarViews() {
+        edTipoEvento = findViewById(R.id.edtxt_meuevento_tipo);
+        edNomeEvento = findViewById(R.id.edtxt_meuevento_nome);
+        edDataInicioEvento = findViewById(R.id.edtxt_meuevento_data_inicio);
+        edDataFimEvento = findViewById(R.id.edtxt_meuevento_data_fim);
 
-    @Override
-    public void onUpdate(Evento evento) {
-        this.evento = evento;
-    }
+        txtEnderecoLocalEvento = findViewById(R.id.txt_meuevento_local_endereco);
+        txtBairroLocalEvento = findViewById(R.id.txt_meuevento_local_bairro);
+        txtComplementoLocalEvento = findViewById(R.id.txt_meuevento_local_complemento);
+        txtCidadeLocalEvento = findViewById(R.id.txt_meuevento_local_cidade);
+        txtUfLocalEvento = findViewById(R.id.txt_meuevento_local_uf);
 
-    @Override
-    public void onDelete() {
-        finish();
-    }
-
-    public void finalizarEdicao(View view) {
-    }
-
-    public void cancelarEdicao(View view) {
-    }
-
-    public void abilitarEdicao(View view) {
+        btnNovaAtividade = findViewById(R.id.btn_meuevento_nova_atividade);
+        btnEditar = findViewById(R.id.btn_meuevento_editar);
+        cardLocal = findViewById(R.id.card_meuevento_local);
+        btnsFinalizarCancelar = findViewById(R.id.buttons_finalizar_cancelar);
     }
 }
